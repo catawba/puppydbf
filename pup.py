c8c8f0dc03ed328711e8b1f76c5b9687dc427b42
catawba <32372307+catawba@users.noreply.github.com>
2024-03-27 17:08:43 -0600

    Initial commit

---
+++ pup.py
@@ -0,0 +1,19 @@
+# pup.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   
+
+db = SQLAlchemy(app)
+
+##########################################
+
+class Puppy(db.Model):
+    __tablename__ = 'puppies'    # manual table name choice                                                    
+                                                            
